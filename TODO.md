ed1782b0a2234d5e50630ba257fdc8f8d2fe6a66
Tom Alexa <tom.alexa.fin@gmail.com>
2022-07-20 21:44:32 +0200

    Visualize active page in navbar

---
+++ TODO.md
@@ -0,0 +1,20 @@
+# TODO
+
+## Frontend
+
+- **routes**
+    - todos are listed in the file [***router/index.js***](app/frontend/src/router/index.js)
+
+- **templates**
+    - Not Found
+    - Profile
+    - Competition
+    - Team
+    - Admin of
+    - Create competition
+
+- **authentication**
+
+## Backend
+
+soon...
--- app/frontend/src/components/Navbar.vue
+++ app/frontend/src/components/Navbar.vue
@@ -1,22 +1,71 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import router from '../router'
+
+
+const updateNavbar = () => {
+
+  var back = r.value.options.history.state.back;
+  var current = r.value.options.history.state.current;
+
+  back = back.split("/")
+  current = current.split("/")
+
+  if (current[1] === "login") {
+    active.value = "login";
+  }
+  else if (current[1] === "signup") {
+    active.value = "signUp";
+  }
+  else if (current[1] === "profile") {
+    active.value = "profile"
+  }
+  else if (current[1] === "my") {
+    if (current[2] === "competitions") {
+      active.value = "myCompetitions";
+    }
+    else if (current[2] === "bets") {
+      active.value = "myBets";
+    }
+  }
+  else if (current[1] === "create") {
+    active.value = "create"
+  }
+  else {
+    active.value = ""
+  }
+
+  pages.value.login = isActive("login")
+  pages.value.signUp = isActive("signUp")
+  pages.value.profile = isActive("profile")
+  pages.value.global = isActive("global")
+  pages.value.myCompetition = isActive("myCompetitions")
+  pages.value.myBets = isActive("myBets")
+  pages.value.create = isActive("create")
+
+}
 
 const isActive = (name) => {
-  if (name === "") {
+  if (name === active.value) {
     return true;
   };
   return false;
 }
 
+const active = ref(String)
 const pages = ref({
-  login: isActive("login"),
-  signUp: isActive("signUp"),
-  profile: isActive("profile"),
-  global: isActive("global"),
-  myCompetition: isActive("myCompetition"),
-  myBets: isActive("myBets"),
-  create: isActive("create"),
+  login: false,
+  signUp: false,
+  profile: false,
+  global: false,
+  myCompetition: false,
+  myBets: false,
+  create: false,
 });
+
+const r = ref(router)
+updateNavbar()
+watch(r.value, updateNavbar)
 
 
 const styleActive = "link-light"
@@ -30,7 +79,7 @@ const styleActive = "link-light"
         <div class="container-fluid">
           <div>
             <router-link to="/" class="navbar-brand link-warning h1 me-4">
-              <img src="/public/money.png" alt="logo" width="40" height="30" class="m-1" />
+              <img src="/money.png" alt="logo" width="40" height="30" class="m-1" />
               GymjiBet
             </router-link>
             <router-link to="/global/competitions" class="navbar-brand" :class="[pages.global ? styleActive : '']">
